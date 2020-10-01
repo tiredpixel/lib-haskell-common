bb365283634f8db631147b9c90de35aae0f65958
Nic Williams <tp@tiredpixel.com>
2020-10-01 13:53:39 +0000

    [#2159] Net,Test: add deps for Elasticsearch plugin

---
+++ src/TPX/Com/Net.hs
@@ -0,0 +1,43 @@
+module TPX.Com.Net (
+    Conn,
+    jsonNDReq,
+    makeReq,
+    makeRes,
+    openConn
+    ) where
+
+
+import              Network.HTTP.Conduit
+import              Network.HTTP.Types
+import              Network.URI
+
+
+type Conn = Manager
+
+jsonNDReq :: Request -> Request
+jsonNDReq req = req {
+    requestHeaders =
+        ("Content-Type", "application/x-ndjson") : requestHeaders req }
+
+makeReq :: ByteString -> URI -> LByteString -> Request
+makeReq verb url body = setRequest verb body $ parseRequest_ $ show' url
+
+makeRes :: Request -> Conn -> IO (Response LByteString)
+makeRes = httpLbs
+
+openConn :: IO Manager
+openConn = newManager tlsManagerSettings
+
+
+setRequest :: ByteString -> LByteString -> Request -> Request
+setRequest verb body req = req {
+    method          = verb,
+    requestBody     = RequestBodyLBS body,
+    redirectCount   = 0,
+    requestVersion  = http11,
+    responseTimeout = responseTimeoutMicro timeout}
+    where
+        timeout = 30000000 -- 30 s
+
+show' :: URI -> String
+show' u = uriToString id u ""
--- test/TPX/Com/API/Test.hs
+++ test/TPX/Com/API/Test.hs
@@ -1,13 +1,15 @@
 module TPX.Com.API.Test (
     module E,
     assertElemN,
-    postJSON
+    postJSON,
+    shouldContainSubseq
     ) where
 
 
 import              Control.Lens                            as  E
 import              Data.Aeson                              as  E   hiding  ((.=))
 import              Data.Aeson.Lens                         as  E
+import              Data.List                               (isSubsequenceOf)
 import              Snap.Core                               as  E   hiding  (addHeader, setContentType, setHeader)
 import              Snap.Test                               as  E
 import              TPX.Com.API.Aeson                       as  E
@@ -21,3 +23,6 @@ assertElemN res n = do
 
 postJSON :: (MonadIO m, ToJSON a) => ByteString -> a -> RequestBuilder m ()
 postJSON u p = postRaw u "application/json" $ toStrict $ encode p
+
+shouldContainSubseq :: Eq a => [a] -> [a] -> Expectation
+shouldContainSubseq a b = (b `isSubsequenceOf` a) `shouldBe` True
