1534d08a4c80ca59fc81dec0846e9c308ebe43d0
Nic Williams <tp@tiredpixel.com>
2020-10-08 15:04:55 +0000

    [#2150] API/Test: add logReq, logReq', logRes

--- test/TPX/Com/API/Test.hs
+++ test/TPX/Com/API/Test.hs
@@ -1,6 +1,9 @@
 module TPX.Com.API.Test (
     module E,
     assertElemN,
+    logReq ,
+    logReq',
+    logRes,
     postJSON,
     shouldContainSubseq
     ) where
@@ -12,8 +15,11 @@ import              Data.Aeson.Lens                         as  E
 import              Data.List                               (isSubsequenceOf)
 import              Snap.Core                               as  E   hiding  (addHeader, setContentType, setHeader)
 import              Snap.Test                               as  E
+import              System.Directory
+import              System.FilePath                         as  FP
 import              TPX.Com.API.Aeson                       as  E
 import              Test.Hspec                              as  E
+import qualified    Data.ByteString.Char8                   as  C8
 
 
 assertElemN :: Response -> Int -> IO ()
@@ -21,8 +27,51 @@ assertElemN res n = do
     b <- getResponseBody res
     length (b ^. _Object) `shouldBe` n
 
+logReq :: FilePath -> RequestBuilder IO () -> IO ()
+logReq tag req = do
+    req' <- buildRequest req
+    req'' <- requestToString req'
+    logReq' tag req''
+
+logReq' :: FilePath -> ByteString -> IO ()
+logReq' tag req = do
+    createDirectoryIfMissing True $ takeDirectory f
+    let (header, body) = splitLog req
+    writeFileBS (f FP.<.> logExtH) header
+    writeFileBS (f FP.<.> logExtB) body
+    where
+        f = logDir </> tag
+
+logRes :: FilePath -> Response -> IO ()
+logRes tag res = do
+    createDirectoryIfMissing True $ takeDirectory f
+    -- HACK: show res not responseToString, as that doubles body
+    -- https://github.com/snapframework/snap-core/issues/233#issuecomment-258780775
+    let (header, body) = splitLog $ show res
+    writeFileBS (f FP.<.> logExtH) header
+    writeFileBS (f FP.<.> logExtB) body
+    where
+        f = logDir </> tag
+
 postJSON :: (MonadIO m, ToJSON a) => ByteString -> a -> RequestBuilder m ()
 postJSON u p = postRaw u "application/json" $ toStrict $ encode p
 
 shouldContainSubseq :: Eq a => [a] -> [a] -> Expectation
 shouldContainSubseq a b = (b `isSubsequenceOf` a) `shouldBe` True
+
+
+logDir :: FilePath
+logDir = "test/log"
+
+logExtH :: FilePath
+logExtH = ".header"
+
+logExtB :: FilePath
+logExtB = ".body"
+
+splitLog :: ByteString -> (ByteString, ByteString)
+splitLog log = (header, body')
+    where
+        sep = "\r\n\r\n"
+        (header, body) = C8.breakSubstring sep log
+        body' = fromMaybe "" $ C8.stripPrefix sep body
