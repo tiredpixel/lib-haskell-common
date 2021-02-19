1e9bb9db3f233963db123e9e8538c0e1f5612c93
Nic Williams <tp@tiredpixel.com>
2021-02-19 16:56:33 +0000

    [#2321] Snap/Test*: import from PE

--- test/TPX/Com/Snap/Test.hs
+++ test/TPX/Com/Snap/Test.hs
@@ -2,19 +2,21 @@ module TPX.Com.Snap.Test (
     module Control.Lens,
     module Data.Aeson,
     module Data.Aeson.Lens,
+    module Prelude,
     module Snap.Core,
     module Snap.Test,
-    module TPX.Com.Snap.CoreUtils,
     module TPX.Com.Snap.TestUtils,
     module Test.Hspec,
+    module Test.Hspec.Snap,
     ) where
 
 
-import              Control.Lens
-import              Data.Aeson                              hiding ((.=))
-import              Data.Aeson.Lens
-import              Snap.Core                               hiding (addHeader, setContentType, setHeader)
-import              Snap.Test
-import              TPX.Com.Snap.CoreUtils
-import              TPX.Com.Snap.TestUtils
-import              Test.Hspec
+import Control.Lens
+import Data.Aeson             hiding ((.=))
+import Data.Aeson.Lens
+import Prelude                hiding (One, get, put, uncons, (??))
+import Snap.Core              hiding (addHeader, pass, setContentType, setHeader)
+import Snap.Test              hiding (getResponseBody)
+import TPX.Com.Snap.TestUtils
+import Test.Hspec             (Spec, SpecWith, describe, it)
+import Test.Hspec.Snap        (SnapHspecM, SnapHspecState, eval, snap)
--- test/TPX/Com/Snap/TestUtils.hs
+++ test/TPX/Com/Snap/TestUtils.hs
@@ -1,43 +1,60 @@
 module TPX.Com.Snap.TestUtils (
-    assertElemN,
-    assertNoContent,
+    addHeaderAuth,
+    defaultPL1,
+    defaultPL2,
+    emptyO,
+    emptyP,
     getLink,
-    linkQValue,
+    getResponseBody,
+    insertLink,
     logReq ,
     logReq',
     logRes,
     postJSON,
-    shouldContainSubseq,
-    testResources,
+    putJSON,
+    readProcessText,
+    runRequest,
+    shouldBe,
+    shouldContain,
+    shouldMeasure,
+    shouldSatisfy,
     ) where
 
 
-import              Control.Lens                            hiding ((<.>))
-import              Data.Aeson
-import              Data.Aeson.Lens
-import              Network.URI
-import              Prelude                                 hiding (put)
-import              Snap.Core                               hiding (addHeader, setContentType, setHeader)
-import              Snap.Test
-import              System.Directory
-import              System.FilePath
-import              Test.Hspec
-import qualified    Data.ByteString                         as  B
-import qualified    Data.ByteString.Char8                   as  C8
-import qualified    Data.List                               as  L
-import qualified    Network.HTTP.Link                       as  HTTP
-import qualified    Relude.Unsafe                           as  Unsafe
+import           Data.Aeson
+import           Network.URI
+import           Prelude                hiding (put)
+import           Snap.Core              hiding (addHeader, setHeader)
+import           Snap.Test              hiding (getResponseBody)
+import           System.Directory
+import           System.FilePath
+import           System.Process
+import qualified Control.Monad.State    as St
+import qualified Data.ByteString.Base64 as B64
+import qualified Data.ByteString.Char8  as C8
+import qualified Data.HashMap.Strict    as HM
+import qualified Data.Map               as M
+import qualified Data.Text              as T
+import qualified Network.HTTP.Link      as HTTP
+import qualified Snap.Test              as ST
+import qualified Test.Hspec.Core.Spec   as Hs
+import qualified Test.Hspec.Snap        as Hs
 
 
-assertElemN :: Response -> Int -> IO ()
-assertElemN res n = do
-    b <- getResponseBody res
-    length (b ^. _Object) `shouldBe` n
+addHeaderAuth :: Monad m => ByteString -> RequestBuilder m ()
+addHeaderAuth b = addHeader "Authorization" $ "Basic " <> B64.encode b
 
-assertNoContent :: Response -> IO ()
-assertNoContent res = do
-    rspStatus res `shouldBe` 204
-    assertElemN res 0
+defaultPL1 :: Params
+defaultPL1 = M.fromList [("_lim", ["1"])]
+
+defaultPL2 :: Params
+defaultPL2 = M.fromList [("_lim", ["2"])]
+
+emptyO :: Object
+emptyO = HM.empty
+
+emptyP :: Params
+emptyP = M.empty
 
 getLink :: HasHeaders a => a -> Maybe (ByteString, ByteString, ByteString)
 getLink res = do
@@ -51,18 +68,22 @@ getLink res = do
     where
         c = show
 
-linkQValue :: ByteString -> ByteString -> ByteString
-linkQValue p = B.drop 1 .
-    snd . B.breakSubstring "=" . snd . B.breakSubstring p
+getResponseBody :: MonadIO m => Response -> m ByteString
+getResponseBody = liftIO . ST.getResponseBody
 
-logReq :: FilePath -> RequestBuilder IO () -> IO ()
-logReq tag req = do
+insertLink :: ByteString -> Params -> Params
+insertLink q = M.insert k [C8.drop 1 v]
+    where
+        (k, v) = C8.break (== '=') $ takeWhileEnd (/= '?') q
+
+logReq :: MonadIO m => FilePath -> RequestBuilder IO () -> m ()
+logReq tag req = liftIO $ do
     req' <- buildRequest req
     req'' <- requestToString req'
     logReq' tag req''
 
-logReq' :: FilePath -> ByteString -> IO ()
-logReq' tag req = do
+logReq' :: MonadIO m => FilePath -> ByteString -> m ()
+logReq' tag req = liftIO $ do
     createDirectoryIfMissing True $ takeDirectory f
     let (header, body) = splitLog req
     writeFileBS (f <.> logExtH) header
@@ -70,8 +91,8 @@ logReq' tag req = do
     where
         f = logDir </> tag
 
-logRes :: FilePath -> Response -> IO ()
-logRes tag res = do
+logRes :: MonadIO m => FilePath -> Response -> m ()
+logRes tag res = liftIO $ do
     createDirectoryIfMissing True $ takeDirectory f
     -- HACK: show res not responseToString, as that doubles body
     -- https://github.com/snapframework/snap-core/issues/233#issuecomment-258780775
@@ -84,16 +105,44 @@ logRes tag res = do
 postJSON :: (MonadIO m, ToJSON a) => ByteString -> a -> RequestBuilder m ()
 postJSON u p = postRaw u "application/json" $ toStrict $ encode p
 
-shouldContainSubseq :: Eq a => [a] -> [a] -> Expectation
-shouldContainSubseq a b = (b `L.isSubsequenceOf` a) `shouldBe` True
+putJSON :: (MonadIO m, ToJSON a) => ByteString -> a -> RequestBuilder m ()
+putJSON u p = put u "application/json" $ toStrict $ encode p
 
-testResources :: Eq t => Response -> [t] ->
-    (Value -> Maybe t) -> (t -> Value -> IO ()) -> [Int] -> IO ()
-testResources res rIds h t es = do
-    b <- getResponseBody res
-    let rIdsB = catMaybes $ h <$> b ^.. values
-    catMaybes (flip L.elemIndex rIds <$> rIdsB) `shouldBe` es
-    for_ (zip [0..] es) $ \(i,j) -> t (rIds Unsafe.!! j) (b ^?! nth i)
+readProcessText :: Text -> [Text] -> Text -> IO Text
+readProcessText cmd args input = toText <$>
+    readProcess (toString cmd) (toString <$> args) (toString input)
+
+runRequest :: RequestBuilder IO () -> Hs.SnapHspecM b Response
+runRequest req = do
+  (Hs.SnapHspecState _ site app is _ bef aft) <- St.get
+  res_ <- liftIO $ Hs.runHandlerSafe req (bef >> site >> aft) app is
+  case res_ of
+    Left err  -> error err
+    Right res -> return res
+
+shouldBe :: (Eq a, Show a) => a -> a -> Hs.SnapHspecM b ()
+shouldBe a e = if a == e
+    then setPass
+    else setFail $ show a <> " /= " <> show e
+infix 1 `shouldBe`
+
+shouldContain :: Text -> Text -> Hs.SnapHspecM b ()
+shouldContain s e = if e `T.isInfixOf` s
+    then setPass
+    else setFail $ show e <> " notMember " <> show s
+infix 1 `shouldContain`
+
+shouldMeasure :: (Foldable t, Show (t a)) => t a -> Int -> Hs.SnapHspecM b ()
+shouldMeasure a l = if length a == l
+    then setPass
+    else setFail $ "|" <> show a <> "| /= " <> show l
+infix 1 `shouldMeasure`
+
+shouldSatisfy :: Show a => a -> (a -> Bool) -> Hs.SnapHspecM b ()
+shouldSatisfy a p = if p a
+    then setPass
+    else setFail $ show a <> " unsatisfied"
+infix 1 `shouldSatisfy`
 
 
 logDir :: FilePath
@@ -105,9 +154,18 @@ logExtH = ".header"
 logExtB :: FilePath
 logExtB = ".body"
 
+setFail :: Text -> Hs.SnapHspecM b ()
+setFail = Hs.setResult . Hs.Failure Nothing . Hs.Reason . toString
+
+setPass :: Hs.SnapHspecM b ()
+setPass = Hs.setResult Hs.Success
+
 splitLog :: ByteString -> (ByteString, ByteString)
 splitLog log = (header, body')
     where
         sep = "\r\n\r\n"
         (header, body) = C8.breakSubstring sep log
         body' = fromMaybe "" $ C8.stripPrefix sep body
+
+takeWhileEnd :: (Char -> Bool) -> ByteString -> ByteString
+takeWhileEnd p = C8.reverse . C8.takeWhile p . C8.reverse
